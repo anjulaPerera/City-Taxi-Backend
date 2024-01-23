d9b0b09f89167b1c2ef2a9fbf31177a5a23a42f0
Anjula Perera <anjulaperera98@gmail.com>
2024-01-23 12:26:45 +0530

    distance changes

--- src/end-points/rides-ep.ts
+++ src/end-points/rides-ep.ts
@@ -17,7 +17,6 @@ import upload from "../middleware/upload-images";
 const { ObjectId } = require("mongodb");
 
 export namespace RidesEp {
-
   export async function passengerReservationRide(req: Request, res: Response) {
     (req, res) => {
       const reservationData = req.body;
@@ -30,5 +29,45 @@ export namespace RidesEp {
     };
   }
 
+  class LocationManager {
+    savePassengerLocation(location: { lat: number; lng: number }): void {
+      // Implement the logic to save passenger location
+      console.log("Passenger location saved:", location);
+    }
 
+    getNearbyDrivers(
+      driverLocations: Record<string, { lat: number; lng: number }>
+    ): string[] {
+      // Implement the logic to calculate nearby drivers
+      console.log("Calculating nearby drivers...");
+      const nearbyDrivers: string[] = [];
+
+      for (const driverId in driverLocations) {
+        const driverLocation = driverLocations[driverId];
+        nearbyDrivers.push(driverId);
+      }
+
+      return nearbyDrivers;
+    }
+  }
+
+  export async function getNearbyDrivers(req: Request, res: Response) {
+    // Hardcoded passenger location
+    const passengerLocation = { lat: 7.020801, lng: 79.973153 }; //7.020801, 79.973153
+
+    // Create an instance of PassengerLocationManager
+    const locationManager = new LocationManager();
+    locationManager.savePassengerLocation(passengerLocation);
+
+    // Hardcoded driver locations
+    const driverLocations: Record<string, { lat: number; lng: number }> = {
+      driver1: { lat: 7.019214, lng: 79.97732 }, //7.019214, 79.977320
+      driver2: { lat: 6.987857, lng: 80.001625 }, //6.987857, 80.001625
+    };
+
+    // Calculate nearby drivers
+    const nearbyDrivers = locationManager.getNearbyDrivers(driverLocations);
+
+    res.json({ nearbyDrivers });
+  }
 }
--- src/routes/rides.ts
+++ src/routes/rides.ts
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-
-import { Express } from "express";
-
-import { RidesEp } from "../end-points/rides-ep";
-
-export function initRidesRoutes(app: Express) {
-  /* PUBLIC ROUTES */
-  app.post("/api/public/current-location", RidesEp.passengerReservationRide);
-=======
 import { Express } from "express";
 import { UserEp } from "../end-points/user-ep";
 import axios from "axios";
 import multer from "multer";
-
-
+import { RidesEp } from "../end-points/rides-ep";
 
 export function initRidesRoutes(app: Express) {
-  app.get("/api/auth/get/near-drivers/:userId", UserEp.getNearbyDrivers);
-
->>>>>>> c36413dc4ebef1885cb5bb9f972a9d49b58e3475
+  app.get("/api/public/get/near-drivers", RidesEp.getNearbyDrivers);
+  // app.get("/api/auth/get/near-drivers/:userId", RidesEp.getNearbyDrivers);
+  app.post("/api/public/current-location", RidesEp.passengerReservationRide);
 }
